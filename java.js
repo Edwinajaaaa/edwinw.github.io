8804ff1468ac65f1367d0bd8011208f2acf9fc37
toko <126941923+Edwinajaaaa@users.noreply.github.com>
2023-08-02 21:08:04 +0700

    Delete java.js

--- java.js
+++
@@ -1,9 +0,0 @@
-// Contoh fungsi JavaScript untuk menambahkan interaktivitas
-
-// Fungsi untuk menampilkan pesan saat gambar di klik
-const projects = document.querySelectorAll('.project');
-projects.forEach(project => {
-  project.addEventListener('click', () => {
-    alert(`Anda mengklik proyek: ${project.querySelector('h3').textContent}`);
-  });
-});
